8b1074aea81fc780b8a179e5d0fd1972c538041e
DaniPeralta <dani.peralta.de@gmail.com>
2018-02-21 23:34:16 +0100

    Finished assigment 11 - Add Infinitive Asteroids

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,25 +6,35 @@ public class GameController : MonoBehaviour {
 
 	public GameObject hazard;
 	public Vector3 spawnValues;
-
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
 
 	// Use this for initialization
 	void Start () {
-		SpawnWaves();
+		StartCoroutine (SpawnWaves());
 	}
 
 
-	void SpawnWaves()
+	IEnumerator SpawnWaves()
 	{
-		Vector3 spawnPosition = new Vector3 (Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
-		Quaternion spawnRotation = Quaternion.identity;
-		Instantiate(hazard, spawnPosition, spawnRotation);
+		yield return new WaitForSeconds (startWait);
+
+		while(true)
+		{
+			for(int i = 0; i < hazardCount; i++)
+			{
+				Vector3 spawnPosition = new Vector3 (Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate(hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+		}
+
+
 	}
 
 
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
