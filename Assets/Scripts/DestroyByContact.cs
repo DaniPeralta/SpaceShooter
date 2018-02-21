a36edb292d5140003dd0713ea43db15ab777b9f8
DaniPeralta <dani.peralta.de@gmail.com>
2018-02-21 22:36:10 +0100

    Finished assigment 9 - Creating explosions

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class DestroyByContact : MonoBehaviour 
 {
 
+	public GameObject explosion;
+	public GameObject playerExplosion;
+
 	void OnTriggerEnter(Collider other)
 	{
-		Debug.Log (other.name);
+		
 		if(other.tag == "Boundary")
 		{
 			return;
+		}
+		Instantiate (explosion, transform.position, transform.rotation);
+		if(other.tag == "Player"){
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);	
 		}
 		Destroy (other.gameObject);
 		Destroy (gameObject);
